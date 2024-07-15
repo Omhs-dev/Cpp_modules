bed386e9c107367258ea7d529c32fe7fb6a8e90a
omhs-dev <hsouleyomar@gmail.com>
2024-07-15 20:17:02 +0200

    ex03 done

---
+++ cpp05/ex03/src/Intern.cpp
@@ -0,0 +1,43 @@
+#include "../Includes/Intern.hpp"
+
+Intern::Intern() {}
+
+Intern::Intern(const Intern &copy)
+{
+    *this = copy;
+}
+
+Intern &Intern::operator=(const Intern &copy)
+{
+    (void)copy;
+    return *this;
+}
+
+Intern::~Intern() {}
+
+AForm* Intern::makeForm(const std::string &formName, const std::string &target)
+{
+    std::string formNames[] = {
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"
+    };
+
+    AForm* form[] = {
+        new ShrubberyCreationForm(target),
+        new RobotomyRequestForm(target),
+        new PresidentialPardonForm(target)
+    };
+
+    for (int i = 0; i < 3; ++i)
+    {
+        if (formName == formNames[i])
+        {
+            std::cout << "Intern creates " << formName << " form" << std::endl;
+            return (form[i]);
+        }
+    }
+
+    std::cout << "Error: Form name " << formName << " does not exist." << std::endl;
+    return (NULL);
+}
